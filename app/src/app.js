e048764052275972e17c5321d44a088a69e84a6e
petro1909 <petya-97_97@mail.ru>
2023-02-02 13:00:58 +0300

    feat: update main app file

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,13 @@
 import {db} from "./model/db.js";
-await db.sequelize.sync({force: true});
-await db.user.create({
-  name: "Tom",
-  email: "email",
-  password: "password",
+import cors from "cors";
+import express from 'express';
+import { userRouter } from "./router/userRouter.js";
+
+export const app = express();
+app.use(cors());
+app.use('/users', userRouter);
+
+await db.sequelize.sync({forse: true});
+app.listen(3000, () => {
+  console.log("server is started on port 3000");
 });
