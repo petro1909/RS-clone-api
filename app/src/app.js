ae5515991d68d135c294551555f3e105215a868d
petro1909 <petya-97_97@mail.ru>
2023-02-03 15:14:49 +0300

    feat: add error handling

--- app/src/app.js
+++ app/src/app.js
@@ -1,13 +1,19 @@
-import {db} from "./model/db.js";
+import {db} from "./components/model/db.js";
 import cors from "cors";
 import express from 'express';
-import { userRouter } from "./router/userRouter.js";
+import { userRouter } from "./components/router/userRouter.js";
+import { userBoardRouter } from "./components/router/userBoardRouter.js";
 
 export const app = express();
 app.use(cors());
 app.use('/users', userRouter);
+app.use('/userBoards', userBoardRouter);
+try {
+  await db.sequelize.sync({force: true});
+} catch(err) {
+  console.error(err.name);
+}
 
-await db.sequelize.sync({forse: true});
 app.listen(3000, () => {
   console.log("server is started on port 3000");
 });
