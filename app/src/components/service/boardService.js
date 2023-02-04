be7998f9c5879fa4cc83493914cdc5aaaeee83f2
petro1909 <petya-97_97@mail.ru>
2023-02-04 13:20:49 +0300

    feat: create functions to manage database board table

---
+++ app/src/components/service/boardService.js
@@ -0,0 +1,64 @@
+import { db } from "../model/db";
+
+export default class BoardService {
+
+  async getBoards(filterOptions, sortParamsArray, pageParams) {
+    let boards;
+    try {
+      boards = await db.board.findAll({ where: filterOptions, order: sortParamsArray,  offset: pageParams.offset, limit: pageParams.limit});
+    } catch(err) {
+      throw new Error(err);
+    }
+    return boards;
+  }
+
+  async getBoardById(boardId) {
+    let board;
+    try {
+      board = await db.board.findByPk(boardId);
+    } catch(err) {
+      throw new Error(err);
+    }
+    return board;
+  }
+
+  async createBoard(board, userId) {
+    let createdBoard;
+    try {
+      createdBoard = await db.board.create(board);
+      const boardUser = { boardId: createdBoard.id, userId: userId, role: "ADMIN" };
+      await db.boardUser.create(boardUser);
+    } catch(err) {
+      throw new Error(err);
+    }
+    return createdBoard;
+  }
+
+  async updateBoard(boardId, board) {
+    let updatedBoard;
+    try {
+      updatedBoard = await db.board.update(board, { where: { id: boardId } });
+    } catch(err) {
+      throw new Error(err);
+    }
+    return updatedBoard;
+  }
+
+  async deleteBoard(boardId) {
+    let isDeleted;
+    try {
+      isDeleted = await db.board.destroy({where: {id: boardId}}); 
+    } catch(err) {
+      throw new Error(err);
+    }
+    return isDeleted;
+    // const boardStatuses = await db.status.findAll({ where: { boardId: boardId } });
+    // boardStatuses.forEach(async (boardStatus)=> {
+    //   await db.task.destroy({ where: { statusId: boardStatus.id } });
+    //   await db.status.destroy({ where: { boardId: boardId }});
+    // });
+    // await db.boardUser.destroy({where: {boardId: boardId}});
+    // await db.board.destroy({where: {id: boardId}});
+  }
+
+}
