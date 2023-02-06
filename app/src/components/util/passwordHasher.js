987fda55721c4da0d7b8cbf11c9b81dc4be0fce4
petro1909 <petya-97_97@mail.ru>
2023-02-06 12:26:10 +0300

    refactor: apply eslint rules

--- app/src/components/util/passwordHasher.js
+++ app/src/components/util/passwordHasher.js
@@ -1,11 +1,11 @@
 import bcrypt from "bcrypt";
 export async function hashPassword(unhashedPassword) {
-  const saltRounds = 10;
-  const salt = await bcrypt.genSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(unhashedPassword, salt);
-  return hashedPassword;
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(unhashedPassword, salt);
+    return hashedPassword;
 }
 export async function comparePassword(unhashedPassword, hashedPassword) {
-  const isEqualPasswords = await bcrypt.compare(unhashedPassword, hashedPassword);
-  return isEqualPasswords;
+    const isEqualPasswords = await bcrypt.compare(unhashedPassword, hashedPassword);
+    return isEqualPasswords;
 }
