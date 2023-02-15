e5aa4147a8cb2766db14655f330b99c5fee3c8ff
petro1909 <petya-97_97@mail.ru>
2023-02-15 19:59:40 +0300

    feat: add functions to manage administration route

--- app/src/components/router/adminRouter.js
+++ app/src/components/router/adminRouter.js
@@ -13,11 +13,45 @@ adminRouter.post("/register", parser, async (req, res) => {
     // sign up admin
     await adminController.createUser(req, res);
 });
+
 adminRouter.get("/users", async (req, res) => {
     // get users by any params
+    await adminController.getUsers(req, res);
+});
+adminRouter.get("/users/:id", async (req, res) => {
+    // get users by any params
+    await adminController.getUsers(req, res);
+});
+adminRouter.post("/users", async (req, res) => {
+    // create user
+    await adminController.getUsers(req, res);
+});
+adminRouter.put("/users", async (req, res) => {
+    // update user
     await adminController.getUsers(req, res);
 });
 adminRouter.delete("/users/:id", async (req, res) => {
     // delete user
     await adminController.deleteUser(req, res);
+});
+
+adminRouter.get("/statistics", async (req, res) => {
+    // get users queries statistic
+    await adminController.getUsers(req, res);
+});
+adminRouter.get("/statistics/:id", async (req, res) => {
+    // get users queries statistic
+    await adminController.getUsers(req, res);
+});
+adminRouter.post("/statistics", async (req, res) => {
+    // post user statistics
+    await adminController.getUsers(req, res);
+});
+adminRouter.put("/statistics", async (req, res) => {
+    // post user statistics
+    await adminController.getUsers(req, res);
+});
+adminRouter.delete("/statistics/:id", async (req, res) => {
+    // delete user statistics
+    await adminController.getUsers(req, res);
 });
