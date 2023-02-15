62ed305addc5ad5cf85e1b7601faa8c69ac71553
petro1909 <petya-97_97@mail.ru>
2023-02-15 21:04:22 +0300

    feat: add methods to manage database tables crud operations

---
+++ app/src/components/repository/baseRepository.js
@@ -0,0 +1,60 @@
+import { db } from "../model/db.js";
+
+export default class DbBaseRepository {
+    async getAll(filterOptions = {}, sortParamsArray = [], pageParams = {}) {
+        let entities;
+        try {
+            entities = await db[this.type].findAll({ where: filterOptions, order: sortParamsArray, offset: pageParams.offset, limit: pageParams.limit });
+        } catch (err) {
+            console.log(err);
+            throw new Error(err);
+        }
+        return entities;
+    }
+
+    async getById(id) {
+        let entity;
+        try {
+            entity = await db[this.type].findByPk(id);
+        } catch (err) {
+            throw new Error(err);
+        }
+        return entity;
+    }
+    async create(entity) {
+        let createdEntity;
+        try {
+            createdEntity = await db[this.type].create(entity);
+        } catch (err) {
+            throw new Error(err);
+        }
+        return createdEntity;
+    }
+
+    async update(entity) {
+        let updatedEntityResult;
+        const entityId = entity.id;
+        delete entity.id;
+        try {
+            updatedEntityResult = await db[this.type].update(entity, {
+                where: { id: entityId },
+                returning: true,
+                plain: true,
+            });
+        } catch (err) {
+            throw new Error(err);
+        }
+        const updatedEntity = updatedEntityResult[1].dataValues;
+        return updatedEntity;
+    }
+
+    async delete(id) {
+        let isDeleted;
+        try {
+            isDeleted = await db[this.type].destroy({ where: { id: id } });
+        } catch (err) {
+            throw new Error(err);
+        }
+        return isDeleted;
+    }
+}
