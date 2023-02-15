97f525f5a4d5b99d43e8c5d2b133221a95f77426
petro1909 <petya-97_97@mail.ru>
2023-02-15 20:01:17 +0300

    feat: add functions to manage taskUsers route

---
+++ app/src/components/router/taskUserRouter.js
@@ -0,0 +1,32 @@
+import express from "express";
+import TaskUserController from "../controller/taskUserController.js";
+import { verifyUser } from "../middleware/authService.js";
+
+export const taskUserRouter = express.Router();
+const parser = express.json();
+const taskUserController = new TaskUserController();
+
+taskUserRouter.get("/", verifyUser, async (req, res) => {
+    // get taskUsers of one board status
+    await taskUserController.getTaskUsers(req, res);
+});
+
+taskUserRouter.get("/:id", verifyUser, async (req, res) => {
+    // get one taskUser by id
+    await taskUserController.getTaskUserById(req, res);
+});
+
+taskUserRouter.post("/", verifyUser, parser, async (req, res) => {
+    // create new taskUser
+    await taskUserController.createTaskUser(req, res);
+});
+
+taskUserRouter.put("/", verifyUser, parser, async (req, res) => {
+    // edit one taskUser
+    await taskUserController.updateTaskUser(req, res);
+});
+
+taskUserRouter.delete("/:id", verifyUser, async (req, res) => {
+    // delete one taskUser
+    await taskUserController.deleteTaskUser(req, res);
+});
