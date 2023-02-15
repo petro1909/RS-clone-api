2ba582f3ec36dddd0d9fb1e2d5630d9a6a4c2365
petro1909 <petya-97_97@mail.ru>
2023-02-15 19:56:09 +0300

    feat: add routes

--- app/src/app.js
+++ app/src/app.js
@@ -9,6 +9,9 @@ import { adminRouter } from "./components/router/adminRouter.js";
 import { boardStatusRouter } from "./components/router/boardStatusRouter.js";
 import { boardTaskRouter } from "./components/router/boardTaskRouter.js";
 import fileUpload from "express-fileupload";
+import { boardMarkRouter } from "./components/router/boardMarkRouter.js";
+import { taskUserRouter } from "./components/router/taskUserRouter.js";
+import { taskMarkRouter } from "./components/router/taskMarkRouter.js";
 
 export const staticFilesFolder = path.resolve(process.cwd(), "./src/static");
 export const app = express();
@@ -19,13 +22,16 @@ app.use("/administration", adminRouter);
 app.use("/users", userRouter);
 app.use("/boards", userBoardRouter);
 app.use("/boardUsers", boardUserRouter);
+app.use("/boardMarks", boardMarkRouter);
 app.use("/statuses", boardStatusRouter);
 app.use("/tasks", boardTaskRouter);
+app.use("/taskUsers", taskUserRouter);
+app.use("/taskMarks", taskMarkRouter);
 
 try {
-    await db.sequelize.sync({ force: false });
+    await db.sequelize.sync({ force: true });
 } catch (err) {
-    console.error(err.name);
+    console.error(err);
 }
 
 app.listen(3000, () => {
