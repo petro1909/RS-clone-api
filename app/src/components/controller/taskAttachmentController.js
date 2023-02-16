4b014e1f34800714d98328315927b78a83d7047b
petro1909 <petya-97_97@mail.ru>
2023-02-16 16:57:15 +0300

    feat: add methods to handle taskAttachment route

--- app/src/components/controller/taskAttachmentController.js
+++ app/src/components/controller/taskAttachmentController.js
@@ -1,81 +1,119 @@
 import TaskAttachmentRepository from "../repository/taskAttachmentRepository.js";
-
+import TaskRepository from "../repository/taskRepository.js";
+import sendJsonHttpResponse from "../service/httpMessageService.js";
 import { createFile, deleteFile, getStaticFile } from "../service/fileService.js";
+import { getFilterParams, getPageParams, getSortParamsArray } from "../service/queryParamsParser.js";
 
-export default class UserController {
+export default class TaskAttachmentController {
     taskAttachmentRepository;
     constructor() {
+        this.taskRepository = new TaskRepository();
         this.taskAttachmentRepository = new TaskAttachmentRepository();
     }
 
-    async getUserProfilePicture(req, res) {
-        const userId = req.params.id;
-        let findedUser;
+    async getTaskAttachmentsPaths(req, res) {
+        const queryParams = req.query;
+        const taskId = queryParams.taskId;
+        const filterParams = getFilterParams(queryParams);
+        const pageParams = getPageParams(queryParams);
+        const sortParamsArray = getSortParamsArray(queryParams);
+        let findedTaskAttachments;
         try {
-            findedUser = await this.userRepository.getUser(userId);
+            findedTaskAttachments = await this.taskAttachmentRepository.getAll(filterParams, sortParamsArray, pageParams);
         } catch (err) {
             return res.status(500).send("Database error");
         }
-        const profilePictureName = findedUser.profilePicture;
-        if (!profilePictureName) {
-            return res.status(404).send("User does't have profile picture");
+        let findedTaskAttachmentsPathsPromises = [];
+        let findedTaskAttachmentsPaths = [];
+        const taskFolder = this.taskRepository.getTaskFolder(taskId);
+        findedTaskAttachmentsPathsPromises = findedTaskAttachments
+            .map((attachment) => {
+                if (attachment.type === "LINK") {
+                    return attachment;
+                }
+                const attachmentName = attachment.name;
+                const attachmentPath = getStaticFile(taskFolder, attachmentName);
+                if (!attachmentPath) return null;
+                attachment.path = attachmentPath;
+                return attachment;
+            })
+            .filter((attachment) => attachment !== null);
+        findedTaskAttachmentsPaths = Promise.allSettled(findedTaskAttachmentsPathsPromises);
+        return res.status(200).json(findedTaskAttachmentsPaths);
+    }
+
+    async getTaskAttachmentsPathById(req, res) {
+        const taskAttachmentId = req.params.id;
+        let findedTaskAttachment;
+        try {
+            findedTaskAttachment = await this.taskAttachmentRepository.getById(taskAttachmentId);
+        } catch (err) {
+            return sendJsonHttpResponse(res, 500, "Database error");
         }
-        const userFolder = this.userRepository.getUserFolder(userId);
-        const userPicture = await getStaticFile(userFolder, profilePictureName);
-        if (userPicture) {
-            return res.json({ profilePicture: `/users/${userId}/${profilePictureName}` });
+        if (findedTaskAttachment.type === "LINK") {
+            return res.status(200).json(findedTaskAttachment);
+        }
+        const taskFolder = this.taskRepository.getTaskFolder(findedTaskAttachment.taskId);
+        const attachmentPath = await getStaticFile(taskFolder, findedTaskAttachment.name);
+        if (attachmentPath) {
+            findedTaskAttachment.path = attachmentPath;
+            return res.status(200).json(findedTaskAttachment);
         } else {
-            return res.status(404).send("Can't access file");
+            return sendJsonHttpResponse(res, 404, "Can't access file");
         }
     }
 
-    async uploadUserProfilePicture(req, res) {
-        const id = req.params.id;
-        let findedUser;
-        try {
-            findedUser = await this.userRepository.getUser(id);
-        } catch (err) {
-            return res.status(500).send("Database error");
-        }
-        if (!findedUser) {
-            return res.status(404).send(`user with id ${id} doesn't exist`);
-        }
+    async uploadTaskAttachment(req, res) {
+        const taskId = req.body.taskId;
+        const attachmentType = req.body.type;
+
         if (!req.files) {
             return res.status(400).send("No files were uploaded.");
         }
-        const picture = req.files.profile;
-        let userFolder = this.userRepository.getUserFolder(id);
-
-        const isCreated = createFile(userFolder, picture, true);
-        if (isCreated) {
-            const upatedUser = { profilePicture: picture.name };
-            await this.userRepository.updateUser(id, upatedUser);
-            return res.json({ profilePicture: `/users/${findedUser.id}/${picture.name}` });
-        } else {
-            return res.status(500).send("Can't upload file");
+        const picture = req.files[0];
+        const attachment = { name: picture.name, attachmentType: attachmentType, taskId: taskId };
+        if (!attachmentType === "LINK") {
+            const taskFolder = this.taskRepository.getTaskFolder(taskId);
+            const isCreated = await createFile(taskFolder, picture, false);
+            if (!isCreated) {
+                return res.status(500).send("Can't upload file");
+            }
+        }
+        let createdAttachment;
+        try {
+            createdAttachment = await this.taskAttachmentRepository.create(attachment);
+            return res.status(201).json(createdAttachment);
+        } catch (err) {
+            return sendJsonHttpResponse(res, 500, "Database error");
         }
     }
 
-    async deleteUserProfilePicture(req, res) {
-        const userId = req.params.id;
-        const user = { profilePicture: null };
-        let profilePictureName;
+    async deleteTaskAttachment(req, res) {
+        const taskAttachmentId = req.params.id;
+        let findedTaskAttachment;
         try {
-            const findedUser = await this.userRepository.getUser(userId);
-            if (!findedUser) {
-                return res.status(404).send(`user with id ${req.body.id} doesn't exist`);
+            const findedTaskAttachment = await this.taskAttachmentRepository.getById(taskAttachmentId);
+            if (!findedTaskAttachment) {
+                return res.status(404).send(`such task attachment doesn't exist`);
             }
-            profilePictureName = findedUser.profilePicture;
-            await this.userRepository.updateUser(userId, user);
         } catch (err) {
             return res.status(500).send("Database error");
         }
-        const userFolder = this.userRepository.getUserFolder(userId);
-        const isDeleted = await deleteFile(userFolder, profilePictureName);
-        if (isDeleted) {
-            res.status(204).send("picture deleted");
+        const taskFolder = this.taskRepository.getTaskFolder(findedTaskAttachment.taskId);
+        const isFileDeleted = await deleteFile(taskFolder, findedTaskAttachment.name);
+        if (isFileDeleted) {
+            let isTaskAttachmentDeleted;
+            try {
+                isTaskAttachmentDeleted = await this.taskAttachmentRepository.deleteAttachment(taskAttachmentId);
+            } catch (err) {
+                return sendJsonHttpResponse(res, 500, "Database error");
+            }
+            if (!isTaskAttachmentDeleted) {
+                return sendJsonHttpResponse(res, 404, "such task attachment doesn't exist");
+            }
+            return res.status(204).send(" task attachment picture deleted");
         } else {
-            return res.status(404).send("can't delete picture");
+            return sendJsonHttpResponse(res, 404, "Can't access file");
         }
     }
 }
