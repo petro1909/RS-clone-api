64d9264bf586a9bfd393d59ba59650beb0ea9042
petro1909 <petya-97_97@mail.ru>
2023-02-16 16:57:52 +0300

    refactor: remove delete user method

--- app/src/components/controller/userController.js
+++ app/src/components/controller/userController.js
@@ -109,21 +109,4 @@ export default class UserController {
         }
         res.status(200).json(updatedUser);
     }
-
-    async deleteUser(req, res) {
-        const id = req.params.id;
-        if (!id) {
-            return sendJsonHttpResponse(res, 400, "Id didn't sent");
-        }
-        let isDeleted;
-        try {
-            isDeleted = await this.userRepository.deleteUser(id);
-        } catch (err) {
-            return sendJsonHttpResponse(res, 500, "Database error");
-        }
-        if (!isDeleted) {
-            return sendJsonHttpResponse(res, 404, "such user doesn't exist");
-        }
-        res.status(204).send("user deleted");
-    }
 }
