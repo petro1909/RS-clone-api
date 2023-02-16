931bca0c58673e43d06980a66359e938cf87b472
petro1909 <petya-97_97@mail.ru>
2023-02-16 16:56:46 +0300

    feat: add methods to handle administration route

--- app/src/components/controller/adminController.js
+++ app/src/components/controller/adminController.js
@@ -1,1 +1,44 @@
-export default class AdminController {}
+import UserRepository from "../repository/userRepository.js";
+import sendJsonHttpResponse from "../service/httpMessageService.js";
+
+export default class AdminController {
+    userRepository;
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async updateUser(req, res) {
+        const userId = req.body.id;
+        if (!userId) {
+            return sendJsonHttpResponse(res, 400, "Id didn't sent");
+        }
+        const user = { role: req.body.role };
+        let updatedUser;
+        try {
+            updatedUser = await this.userRepository.updateUser(user);
+        } catch (err) {
+            return sendJsonHttpResponse(res, 500, "Database error");
+        }
+        if (!updatedUser) {
+            return sendJsonHttpResponse(res, 404, "such user doesn't exist");
+        }
+        res.status(200).json(updatedUser);
+    }
+
+    async deleteUser(req, res) {
+        const id = req.params.id;
+        if (!id) {
+            return sendJsonHttpResponse(res, 400, "Id didn't sent");
+        }
+        let isDeleted;
+        try {
+            isDeleted = await this.userRepository.deleteUser(id);
+        } catch (err) {
+            return sendJsonHttpResponse(res, 500, "Database error");
+        }
+        if (!isDeleted) {
+            return sendJsonHttpResponse(res, 404, "such user doesn't exist");
+        }
+        res.status(204).send("user deleted");
+    }
+}
