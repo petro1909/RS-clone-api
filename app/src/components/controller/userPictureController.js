c5c87b01afc82aa8c946903efdb064e96f2ca87b
petro1909 <petya-97_97@mail.ru>
2023-02-16 15:08:35 +0300

    feat: change method to send response messages

--- app/src/components/controller/userPictureController.js
+++ app/src/components/controller/userPictureController.js
@@ -15,18 +15,18 @@ export default class UserPictureController {
         try {
             findedUser = await this.userRepository.getUser(userId);
         } catch (err) {
-            return res.status(500).send("Database error");
+            return sendJsonHttpResponse(res, 500, "Database error");
         }
         const profilePictureName = findedUser.profilePicture;
         if (!profilePictureName) {
-            return res.status(404).send("User doesn't have profile picture");
+            return sendJsonHttpResponse(res, 404, "User doesn't have profile picture");
         }
         const userFolder = this.userRepository.getUserFolder(userId);
         const userPicture = await getStaticFile(userFolder, profilePictureName);
         if (userPicture) {
             return res.json({ profilePicture: `/users/${userId}/${profilePictureName}` });
         } else {
-            return res.status(404).send("Can't access file");
+            return sendJsonHttpResponse(res, 404, "Can't access file");
         }
     }
 
@@ -36,30 +36,34 @@ export default class UserPictureController {
         try {
             findedUser = await this.userRepository.getUser(id);
         } catch (err) {
-            return res.status(500).send("Database error");
+            return sendJsonHttpResponse(res, 500, "Database error");
         }
         if (!findedUser) {
-            return res.status(404).send(`user with id ${id} doesn't exist`);
+            return sendJsonHttpResponse(res, 404, "such user with doesn't exist");
         }
         if (!req.files) {
-            return res.status(400).send("No files were uploaded.");
+            return sendJsonHttpResponse(res, 400, "No files were uploaded");
         }
         const picture = req.files.profile;
         let userFolder = this.userRepository.getUserFolder(id);
 
         const isCreated = createFile(userFolder, picture, true);
         if (isCreated) {
-            const upatedUser = { profilePicture: picture.name };
-            await this.userRepository.updateUser(id, upatedUser);
-            return res.json({ profilePicture: `/users/${findedUser.id}/${picture.name}` });
+            const upatedUser = { id: id, profilePicture: picture.name };
+            try {
+                await this.userRepository.updateUser(upatedUser);
+            } catch (err) {
+                return sendJsonHttpResponse(res, 500, "Database error");
+            }
+            return res.status(200).json({ profilePicture: `/users/${findedUser.id}/${picture.name}` });
         } else {
-            return res.status(500).send("Can't upload file");
+            return sendJsonHttpResponse(res, 500, "Can't upload file");
         }
     }
 
     async deleteUserProfilePicture(req, res) {
         const userId = req.params.id;
-        const user = { profilePicture: null };
+        const user = { id: userId, profilePicture: null };
         let profilePictureName;
         try {
             const findedUser = await this.userRepository.getUser(userId);
@@ -67,7 +71,7 @@ export default class UserPictureController {
                 return sendJsonHttpResponse(res, 404, "user not found");
             }
             profilePictureName = findedUser.profilePicture;
-            await this.userRepository.updateUser(userId, user);
+            await this.userRepository.updateUser(user);
         } catch (err) {
             return sendJsonHttpResponse(res, 500, "Database error");
         }
