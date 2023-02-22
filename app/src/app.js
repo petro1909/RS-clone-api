74d95a6785857015a6da4ae9ce7170398a8a9b1f
petro1909 <petya-97_97@mail.ru>
2023-02-22 19:49:50 +0300

    feat: rework express app adding routers way

--- app/src/app.js
+++ app/src/app.js
@@ -2,18 +2,11 @@ import path from "path";
 import { db } from "./components/database/index.js";
 import cors from "cors";
 import express from "express";
-import { userRouter } from "./components/router/userRouter.js";
-import { userBoardRouter } from "./components/router/userBoardRouter.js";
-import { boardUserRouter } from "./components/router/boardUserRouter.js";
-import { adminRouter } from "./components/router/adminRouter.js";
-import { boardStatusRouter } from "./components/router/boardStatusRouter.js";
-import { boardTaskRouter } from "./components/router/boardTaskRouter.js";
+import fs from "fs/promises";
 import fileUpload from "express-fileupload";
-import { boardMarkRouter } from "./components/router/boardMarkRouter.js";
-import { taskUserRouter } from "./components/router/taskUserRouter.js";
-import { taskMarkRouter } from "./components/router/taskMarkRouter.js";
 import { RequestLoggerService } from "./components/middleware/requestLoggerService.js";
 
+export const routers = new Map();
 const logger = new RequestLoggerService();
 export const staticFilesFolder = path.resolve(process.cwd(), "./src/static");
 export const app = express();
@@ -21,21 +14,28 @@ app.use(cors());
 app.use(fileUpload());
 app.use(logger.makeLog);
 app.use(express.static(staticFilesFolder));
-app.use("/administration", adminRouter);
-app.use("/users", userRouter);
-app.use("/boards", userBoardRouter);
-app.use("/boardUsers", boardUserRouter);
-app.use("/boardMarks", boardMarkRouter);
-app.use("/statuses", boardStatusRouter);
-app.use("/tasks", boardTaskRouter);
-app.use("/taskUsers", taskUserRouter);
-app.use("/taskMarks", taskMarkRouter);
+
+const routerFilesFolderPath = path.resolve(process.cwd(), "./src/components/router");
+const routerFilesFolder = "components/router";
+
+async function mapRouter() {
+    let routerFiles = await fs.readdir(routerFilesFolderPath);
+    routerFiles = routerFiles.filter((file) => file.indexOf(".") !== 0 && file.slice(-3) === ".js" && file.indexOf(".test.js") === -1);
+    routerFiles = routerFiles.map((file) => import(`./${routerFilesFolder}/${file}`));
+    routerFiles = await Promise.all(routerFiles);
+    routerFiles.forEach((routeFile) => {
+        const routeEntity = routeFile.default;
+        app.use(`/${routeEntity.route}`, routeEntity.router);
+    });
+}
+mapRouter();
 
 try {
     await db.sequelize.sync({ force: false });
 } catch (err) {
     console.error(err);
 }
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("server is started on port 3000");
